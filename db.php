81088f555326e730d147cbf6e9d477120cc9be09
juanitoCarnicero99 <141794790+juanitoCarnicero99@users.noreply.github.com>
2025-05-20 20:13:38 -0500

    Update db.php
    .

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "root", "cogela_suave_db");
+$mysqli = new mysqli("sql111.infinityfree.com", "if0_39037484", "JJBautis160r2v", "if0_39037484_cogela_suave_db");
 
 if ($mysqli->connect_error) {
     die("Conexión fallida: " . $mysqli->connect_error);
